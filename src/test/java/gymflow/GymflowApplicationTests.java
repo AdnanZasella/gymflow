66eba9cb61f60eea3f431d83e231f37131a3918d
AdnanZasella <adnanzasella@hotmail.com>
2026-09-11 19:55:46 +0200

    Initial Spring Boot project setup with Docker Postgres

---
+++ src/test/java/gymflow/GymflowApplicationTests.java
@@ -0,0 +1,13 @@
+package gymflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
